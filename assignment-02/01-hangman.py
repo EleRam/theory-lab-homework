5b2d4b658b22949a855e366566d16bb6d30f71f2
Ramin Firooz <eleram.com@gmail.com>
2022-03-05 13:03:13 -1200

    added hangman scaffold

--- assignment-02/01-hangman.py
+++ assignment-02/01-hangman.py
@@ -2,17 +2,87 @@ import random
 
 word_bank = ["python", "java", "kotlin", "javascript"]
 
-word    = random.choice(word_bank)
+word = random.choice(word_bank)
 wordArr = ["-"] * len(word)
 success = False
 userGuesses = []
 userGuess = ""
 wrongGuesses = 0
 
+
+def print_scaffold(wrongs):  # prints the scaffold
+    if (wrongs == 0):
+        print("_________")
+        print("|	 |")
+        print("|")
+        print("|")
+        print("|")
+        print("|")
+        print("|________")
+    elif (wrongs == 1):
+        print("_________")
+        print("|	 |")
+        print("|	 O")
+        print("|")
+        print("|")
+        print("|")
+        print("|________")
+    elif (wrongs == 2):
+        print("_________")
+        print("|	 |")
+        print("|	 O")
+        print("|	 |")
+        print("|	 |")
+        print("|")
+        print("|________")
+    elif (wrongs == 3):
+        print("_________")
+        print("|	 |")
+        print("|	 O")
+        print("|	\|")
+        print("|	 |")
+        print("|")
+        print("|________")
+    elif (wrongs == 4):
+        print("_________")
+        print("|	 |")
+        print("|	 O")
+        print("|	\|/")
+        print("|	 |")
+        print("|")
+        print("|________")
+    elif (wrongs == 5):
+        print("_________")
+        print("|	 |")
+        print("|	 O")
+        print("|	\|/")
+        print("|	 |")
+        print("|	/ ")
+        print("|________")
+    elif (wrongs == 6):
+        print("_________")
+        print("|	 |")
+        print("|	 O")
+        print("|	\|/")
+        print("|	 |")
+        print("|	/ \ ")
+        print("|________")
+        # print("\n")
+        # print("The word was %s." % wd)
+        # print("\n")
+        # print("\nYOU LOSE! TRY AGAIN!")
+        # print("\nWould you like to play again, type 1 for yes or 2 for no?")
+        # again = str(raw_input("> "))
+        # again = again.lower()
+        # if again == "1":
+        #     hangMan()
+        return
+
+
 while wrongGuesses < 6 and success == False:
 
     print("\n" + " ".join(wordArr))
-    
+
     print("Please guess a letter:")
     guess = input()
 
@@ -24,6 +94,7 @@ while wrongGuesses < 6 and success == False:
     else:
         print("Wrong")
         wrongGuesses += 1
+        print_scaffold(wrongGuesses)
         print("You have", 6 - wrongGuesses, "guesses left")
 
     for i in range(len(word)):
@@ -32,9 +103,11 @@ while wrongGuesses < 6 and success == False:
         success = True
         print("You guessed the word!")
         break
-    
+
     if wrongGuesses == 6:
         print("You lose!")
         print("You guessed: " + userGuess)
         print("The word was: " + word)
         break
+
+
